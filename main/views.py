4ac3d89c4d63e23710318ef49cac55172104b738
danille <danilleock@gmail.com>
2016-05-19 18:13:30 +0200

    v 0.0.1

--- main/views.py
+++ main/views.py
@@ -6,28 +6,7 @@ from django.http import JsonResponse
 from .scripts.calculation import calculate
 
 
-# Create your views here.
-# @api_view(['GET', 'POST'])
-# def get_advice(request):
-#     key = request.query_params.get('key')
-#     danil_key = 'danil_key'
-#     if request.method == 'GET' and key != danil_key:
-#
-#         # Retrieving data from User: fetching URL-query parameters
-#         sex = request.query_params.get('s')  # User's sex
-#         req_lat = request.query_params.get('lat')  # User's current latitude
-#         req_lon = request.query_params.get('lon')  # User's current latitude
-#         fav_color = request.query_params.get('f_col')  # User's favourite color
-#         dis_color = request.query_params.get('d_col')  # User's unloved color
-#         day_index = int(request.query_params.get('d_ind'))  # Day required by User : 0 - today, 8 - week later
-#         # Retrieving weather forecast from OpenWeatherMap API for required day and
-#         weather = get_weather(req_lat, req_lon, day_index)
-#         return Response({'sex': sex, 'weather': weather}, status=status.HTTP_200_OK)
-#     else:
-#         return Response({'key': 'value'}, status=status.HTTP_200_OK)
-#
 def get_advice(request):
-    # key = request.GET.get('key')
     lat = request.GET.get('lat')
     lon = request.GET.get('lon')
     fav_color = request.GET.get('f_col')
@@ -59,8 +38,3 @@ def get_weather(lat, lon, day_index):
         average_daily_temp = float(weather['list'][day_index]['temp']['day'])  # Average temperature in required day
         weather_conditions = int(weather['list'][day_index]['weather'][0]['id'])  # Rain params in required day
         return average_daily_temp, weather_conditions
-
-#
-# class AdviceViewSet(viewsets.ModelViewSet):
-#     queryset = Advice.objects.all()
-#     serializer_class = AdviceSerializer
